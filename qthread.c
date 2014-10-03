d30cd6cab6568c1de889b77bb997c177a394543d
Ahmad <ahmad@Ahmads-MacBook-Air.local>
2014-10-03 14:20:29 -0400

    printing os thread values

--- qthread.c
+++ qthread.c
@@ -257,10 +257,13 @@ void allocateThreadStack(void** basePtr, void** offsetPtr) {
 }
 
 void createAndSetupTCB(qthread_t currentTCB) {
+    printf("in setup and create stack");
 
 	currentTCB = (qthread_t)malloc(sizeof(struct qthread)); 
 	insertTCB(currentTCB);
-	allocateThreadStack(currentTCB->basePtr, currentTCB->offsetPtr);	
+    printf("%p\n", currentTCB->basePtr);
+    printf("%p\n", currentTCB->offsetPtr);
+	allocateThreadStack(&currentTCB->basePtr, &currentTCB->offsetPtr);	
         currentTCB->detached = 0;
         currentTCB->status = 1;
         currentTCB->prev = NULL;
