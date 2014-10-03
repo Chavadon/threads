0c40b7fa1e0c4ba489147fdd217ed836e66fddaf
Ahmad <ahmad@Ahmads-MacBook-Air.local>
2014-10-03 15:52:12 -0400

    printing os thread values

--- qthread.c
+++ qthread.c
@@ -308,10 +308,10 @@ int qthread_create(qthread_t *thread, qthread_attr_t *attr,
     printf("%p\n", os_thread.basePtr);
     printf("%p\n", os_thread.offsetPtr);
 
-    qthread_t newTCB = thread;
+    //qthread_t newTCB = *thread;
 
-    createAndSetupTCB(newTCB);
-    setup_stack(newTCB->offsetPtr, NULL,NULL, NULL);
+    createAndSetupTCB(thread);
+    setup_stack(thread->offsetPtr, NULL,NULL, NULL);
 
     qthread_yield();
 
